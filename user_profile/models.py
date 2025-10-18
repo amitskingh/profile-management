95b63b56917c302fe02f17d238a5b5681f1a6e0d
Amit Singh <h4775990@gmail.com>
2025-10-18 14:47:32 +0530

    update

---
+++ user_profile/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    country = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    profile_picture = models.ImageField(upload_to='profile_pictures/')
+    education = models.CharField(max_length=100)
+    skills = models.TextField()
+    bio = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
